4b84b69b02672f086d1d919c03813ad6a364e553
baong <baong@vietravel.com>
2025-03-11 08:20:38 +0700

    update webhook URL in submitForm.service.ts

--- src/api/submitForm.service.ts
+++ src/api/submitForm.service.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 
 // const WEBHOOK_URL = "https://hiengiabao12.app.n8n.cloud/webhook-test/109b1dd8-0f10-43ca-842c-a6a8a1a706ab";
 // const WEBHOOK_URL = "https://hiengiabao12.app.n8n.cloud/webhook/109b1dd8-0f10-43ca-842c-a6a8a1a706ab";
-const WEBHOOK_URL = "http://108.108.110.102:5678/webhook/66e40490-923c-4eb8-af8d-00cc04c82851";
+// const WEBHOOK_URL = "http://108.108.110.102:5678/webhook/66e40490-923c-4eb8-af8d-00cc04c82851";
+const WEBHOOK_URL = "https://baong.app.n8n.cloud/webhook/defaba54-b39d-4fe3-9306-f07ec0205c51"
+
 
 export const submitForm = async (data: any) => {
   try {
